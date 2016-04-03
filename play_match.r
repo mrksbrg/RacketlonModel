5c3b686c0cd8706ea5c52a41f770a6bee79b0826
Markus Borg <markus.borg@gmail.com>
2016-04-03 23:00:24 +0200

    Fixed lost TT uncertainty
    Added the lost uncertainty in TT, in line with other sports.

--- play_match.r
+++ play_match.r
@@ -1,7 +1,7 @@
 # result <- play_match(100000, 15687, 17065, 16077, 14351, 16323, 16928, 16039, 14379) # Borg - Hellberg
 # result <- play_match(100000, 15687, 17065, 16077, 14351, 17031, 21081, 19962, 16985) # Borg - Ratzer
-# result <- play_match(100000, 15532, 15712, 16025, 15247, 16323, 16928, 16039, 14379) # Åhlander - Hellberg
-# result <- play_match(100000, 17031, 21081, 19962, 16985, 17510, 19942, 18641, 16764) # Ratzer - Jønsson
+# result <- play_match(100000, 15532, 15712, 16025, 15247, 16323, 16928, 16039, 14379) # Ã…hlander - Hellberg
+# result <- play_match(100000, 17031, 21081, 19962, 16985, 17510, 19942, 18641, 16764) # Ratzer - JÃ¸nsson
 play_match <- function(n_sim, p1_tt, p1_ba, p1_sq, p1_te, p2_tt, p2_ba, p2_sq, p2_te) {
   result_tt <- 1:n_sim
   result_ba <- 1:n_sim
@@ -18,7 +18,7 @@ play_match <- function(n_sim, p1_tt, p1_ba, p1_sq, p1_te, p2_tt, p2_ba, p2_sq, p
     # table tennis
     p1_performance <- rnorm(1, p1_tt, 100)
     p2_performance <- rnorm(1, p2_tt, 100)
-    result_tt[i] <- play_tt_set(p1_tt, p2_tt)
+    result_tt[i] <- play_tt_set(p1_performance, p2_performance)
     diff[i] <- result_tt[i]
     
     # badminton
